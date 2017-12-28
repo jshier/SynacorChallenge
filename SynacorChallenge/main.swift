32c6b66b0c13aa8d543a32d917ead5e2e25681f1
Jon Shier <jon@jonshier.com>
2017-12-28 14:54:14 -0600

    Initial commit.

---
+++ SynacorChallenge/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SynacorChallenge
+//
+//  Created by Jon Shier on 12/28/17.
+//  Copyright © 2017 Jon Shier. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
